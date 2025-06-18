9c7c4d2715587b7902c353c1a8dc2c596c326838
Aleksandr Koshelev <ko4evneg@gmail.com>
2025-06-18 10:45:29 +0300

    Refactor

--- src/main/java/com/github/ko4evneg/ScratchGame.java
+++ src/main/java/com/github/ko4evneg/ScratchGame.java
@@ -11,7 +11,6 @@ public class ScratchGame {
     public static void main(String... args) {
         AppConfig appConfig = new AppConfig(args);
         GameConfig gameConfig = ScratchSerializer.deserialize(appConfig.getConfigPath());
-        //TODO validate probabilities; first cell symbol; win combinations count < overall
         Game game = new Game(gameConfig, appConfig.getBettingAmount(), new Random());
         GameResult result = game.start();
         String textResult = ScratchSerializer.serialize(result);
